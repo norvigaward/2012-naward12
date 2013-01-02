3ec82190e7d235fc4f21b70308ba98ecf728ac5f
Norvig Award Participant <participant@hadoop-vm.(none)>
2013-01-02 21:16:18 +0100

    nieuwe files

---
+++ testscripts/script3.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+
+sub doNothingWithHtml
+{
+	my ($html) = @_;
+	my $errorList = "";
+	my $range = 10;
+	my $forLoop = int(rand($range));
+	$range = 399;
+	for ($forLoop; $forLoop>= 1; $forLoop--)
+	{
+		$errorList .= (int(rand($range))+1);
+		$errorList .= ";";
+	}
+	return $errorList;
+}
+
+my $print = doNothingWithHtml($ARGV[0]);
+print "$print";
