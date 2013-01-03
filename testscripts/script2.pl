ba9b8cd933bbbfc5f5644b84ab29173e96387c69
Reuel <reuelbrion@gmail.com>
2013-01-03 16:18:24 +0100

    Update testscripts/script2.pl

--- testscripts/script2.pl
+++ testscripts/script2.pl
@@ -17,14 +17,14 @@ while(<>)
         s/\s+/ /g; #substitute one or more spaces for a space and do this for all match$
         s/\s$//; #substitue a new line for nothing
         $html = "$_\n";
-        $errorCodeTabVersie = subScript($html); #dit is een variabele met errocodes met ; ertussen en dan een tab gevolgd door de HTML versie
+        $errorCodeMetVersie = subScript($html); #dit is een variabele met errocodes met ; ertussen en dan een tab gevolgd door de HTML versie
     if($html =~ /"<\s*a\s+href\s*=\s*"\s*http:\/\/validator.w3.org\/check\?uri=referer\s*"\s*>\s*<\s*a\s+href\s*=\s*"\s*http:\/\/validator\.w3\.org\/check\?uri\=referer\s*"\s*>\s*<\s*img\s+src\s*=\s*"\s*http:\/\/www.w3.org\/Icons\/[^\/]+"\s*alt\s*=\s*"[^\"]+"\s*height\s*=\s*"\s*[0-9]+\s*"\s*width\s*=\s*"\s*\d+\s*"\s*\/>\s*<\s*\/a\s*>"/i)
     {
         $html_picture = 1;
     } else {
         $html_picture = 0;
     }
-        $versie = "100;";
-        print "$url\t$errorCodeTabVersie\t$versie\t$html_picture\n"; #print $_ dan tab dan $error code
+        @output = split(/-/, $errorCodeMetVersie);
+        print "$url\t$output[0]\t$output[1]\t$html_picture\n"; #print $_ dan tab dan $error code
         $/ = "\t";
 }
