70a2b354ee44ac312cd69f4f8c652a6fb9cacace
Reuel <reuelbrion@gmail.com>
2013-01-03 17:17:58 +0100

    Update testscripts/script3.pl

--- testscripts/script3.pl
+++ testscripts/script3.pl
@@ -3,19 +3,20 @@
 sub doNothingWithHtml
 {
 	my ($html) = @_;
-	my $errorList = ";";
+	my $errorList = "";
 	my $range = 10;
 	my $forLoop = int(rand($range));
+	print "$forLoop";
 	$range = 489;
-	for ($forLoop; $forLoop>= 1; $forLoop--)
+	for ($i = $forLoop; $i >= 1; $i--)
 	{
-		$errorList .= (int(rand($range))+1);
-		$errorList .= ";";
+		$errorList.= (int(rand($range))+1);
+		$errorList.= ";";
 	}
-	$errorList .= "-";
+	$errorList.= "-";
 	my @htmlArray = ("HTML1", "HTML2", "HTML3", "XHTML1");
 	$range = 4;
-	$errorList .= $htmlArray[int(rand($range))];
+	$errorList.= $htmlArray[int(rand($range))];
 	return $errorList;
 }
 
