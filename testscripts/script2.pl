eac01529609432805d2b0133fb18004c3e0281b2
Reuel <reuelbrion@gmail.com>
2013-01-03 16:56:48 +0100

    Update testscripts/script2.pl

--- testscripts/script2.pl
+++ testscripts/script2.pl
@@ -17,7 +17,7 @@ while(<>)
         s/\s+/ /g; #substitute one or more spaces for a space and do this for all match$
         s/\s$//; #substitue a new line for nothing
         $html = "$_\n";
-        $errorCodeMetVersie = subScript($html); #dit is een variabele met errocodes met ; ertussen en dan een tab gevolgd door de HTML versie
+        $errorCodeMetVersie = subScript($html); #dit is een variabele met errocodes met ; ertussen de HTML versie
     if($html =~ /"<\s*a\s+href\s*=\s*"\s*http:\/\/validator.w3.org\/check\?uri=referer\s*"\s*>\s*<\s*a\s+href\s*=\s*"\s*http:\/\/validator\.w3\.org\/check\?uri\=referer\s*"\s*>\s*<\s*img\s+src\s*=\s*"\s*http:\/\/www.w3.org\/Icons\/[^\/]+"\s*alt\s*=\s*"[^\"]+"\s*height\s*=\s*"\s*[0-9]+\s*"\s*width\s*=\s*"\s*\d+\s*"\s*\/>\s*<\s*\/a\s*>"/i)
     {
         $html_picture = 1;
