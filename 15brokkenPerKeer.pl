a958f11e9da3e53a2b31fb0de1b03f6a8036fa27
Reuel <reuelbrion@gmail.com>
2013-01-13 14:51:04 +0100

    Update 15brokkenPerKeer.pl

--- 15brokkenPerKeer.pl
+++ 15brokkenPerKeer.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
 
-@files = <*>;
+my @files = `hadoop fs -ls hdfs://p-head03.alley.sara.nl/data/public/common-crawl/parse-output/segment/1346823845675`;
+
+#@files = <*>;
 foreach $file (@files)
 {
   print $file;
