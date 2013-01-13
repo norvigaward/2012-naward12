c22fffe746ed23a9b980491c6720a68b8e07d3fd
Reuel <reuelbrion@gmail.com>
2013-01-13 16:56:57 +0100

    Update 15brokkenPerKeer.pl

--- 15brokkenPerKeer.pl
+++ 15brokkenPerKeer.pl
@@ -88,20 +88,23 @@ while($boole)
       $loadList.=", ";
     $loadList.=$files[$teller];
   }
-  if (++$teller > $arrayLength)
-  {
+  #if (++$teller > $arrayLength)
+  #{
     $boole = 0;
-  }
+  #}
+    
+my $pigString = "register '//home//participant//git//commoncrawl-examples//lib//*.jar';
+register '//home//participant//git//commoncrawl-examples//dist//lib//commoncrawl-examples-1.0.1-HM.jar'; a = LOAD '$loadList' USING org.commoncrawl.pig.ArcLoader() AS (charset: chararray, length, type: chararray, statuscode, ipaddress, url: chararray, html);
+b = FILTER a BY type == 'text//html';
+c = foreach b generate url, charset, REPLACE (html, '\n', ' ') as html;
+define myscript `test.sh` input (stdin using PigStreaming('\t')) output (stdout) ship('//home//participant//git//naward12//test.sh','//home//participant//git//naward12//check3','//home//participant//git//naward12//dtd.tar','//home//participant//git//naward12//charset.cfg', '//home//participant//git//naward12//httpd.conf','//home//participant//git//naward12//validator.conf','//home//participant//git//naward12//types.conf','//home//participant//git//naward12//tips.cfg');
+d = stream c through myscript as (url, errorcode, htmlversion, valid);
+store d into 'didado';";
   
-   
-    my $pigString = "register '//home//participant//git//commoncrawl-examples//lib//*.jar'; 
-    register '//home//participant//git//commoncrawl-examples//dist//lib//commoncrawl-examples-1.0.1-HM.jar'; a = LOAD '$loadList' USING org.commoncrawl.pig.ArcLoader() AS (charset: chararray, length, type: chararray, statuscode, ipaddress, url: chararray, html);
-    b = FILTER a BY type == 'text//html';
-    c = foreach b generate url, charset, REPLACE (html, '\n', ' ') as html;
-    define myscript `test.sh` input (stdin using PigStreaming('\t')) output (stdout) ship('//home//participant//git//naward12//test.sh','//home//participant//git//naward12//check3','//home//participant//git//naward12//dtd.tar','//home//participant//git//naward12//charset.cfg', '//home//participant//git//naward12//httpd.conf','//home//participant//git//naward12//validator.conf','//home//participant//git//naward12//types.conf','//home//participant//git//naward12//tips.cfg');
-    d = stream c through myscript as (url, errorcode, htmlversion, valid);
-    store d into 'didado';";
-  
-  print $pigString; 
+   open(HANDLE,">tmpfile.pig");
+   print HANDLE "$pigString";
+    close (HANDLE);
+   system("pig -f tmpfile.pig");
+    system("rm tmpfile.pig");
 }
     
