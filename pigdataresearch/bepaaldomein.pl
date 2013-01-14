18f4cffc2a83028f23e8552f7972fa8aba3783c0
Reuel <reuelbrion@gmail.com>
2013-01-14 20:17:02 +0100

    Update pigdataresearch/bepaaldomein.pl

--- pigdataresearch/bepaaldomein.pl
+++ pigdataresearch/bepaaldomein.pl
@@ -3,13 +3,17 @@
 #this script extracts the top level domain out of an url
 use URI;
 
+$/ = "\t";
 while(<>)
 {
   my $string1 = $_;
+  $/ = "\n";
+  $_ = <>;
   my $url = URI->new($string1);
   my $domain = $url->host;
   my @domainGehakt = split('\.', $domain);
   my $lengte = scalar(@domainGehakt);
   my $output = $domainGehakt[$lengte - 1];
   print $output;
+  $/ = "\t";
 }
